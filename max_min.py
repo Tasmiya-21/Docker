68be2549a9f30843e7ff1618ffb899bafcf15bfe
Tasmiya-21 <96778279+Tasmiya-21@users.noreply.github.com>
2022-04-04 22:51:02 +0530

    Update max_min.py

--- max_min.py
+++ max_min.py
@@ -1,5 +1,5 @@
 import pytest
-values=eval(input("enter your list: "))
+
 def max(values):
     max=values[0]
     #print(values[0])
@@ -7,7 +7,7 @@ def max(values):
         if val>max:
             max=val
     #print("The maximum number is",max)
-    return "The maximum number is",max
+    return max
     
 def min(values):
     min=values[0]
@@ -15,11 +15,19 @@ def min(values):
         if val<min:
             min=val
     #print("The minimum number is",min)
-    return "The minimum number is",min
+    return min
 def test_max():
+    values=eval(input("enter your list: "))
     #values=[4,87,10,2]
     assert max(values)==int(input("enter your no1:"))
 
 def test_min():
+    values=eval(input("enter your list: "))
     #values=[4,87,10,2]
     assert min(values)==int(input("enter your no2:"))
+
+#values=[4,87,10,2]
+   
+#max(values)
+#min(values)
+#test_method()
